8baff416c2001e1c47d4bb8958c2175c5469f208
Antononlahe <anton.zatkin@gmail.com>
2016-03-15 21:27:40 +0200

    nimegen

--- Prussakad/src/Prussakad.java
+++ Prussakad/src/Prussakad.java
@@ -5,8 +5,10 @@ import java.util.Random;
 public class Prussakad {
 	public static void main(String [] args){
 		System.out.println("kartul");
-		Prussakas juss = Prussakas.loo_prussakas(1);
+		Prussakas [] juss = new Prussakas[50];
+		for (int i = 0; i<50;i++){
+			juss[i] = Prussakas.loo_prussakas(1);
+		}
+		for (Prussakas pruss : juss)System.out.println(pruss.getNimi());
 	}
 }
-
-
