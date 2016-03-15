9e968f8f4e73ce1173b557a55838c500125d6b29
markhenrip <markhenripedoson@gmail.com>
2016-03-15 21:54:51 +0200

    Revert "Baas"
    This reverts commit 18195dabb3eb442a75df0ca911be00531b7ec618.

--- Prussakad/src/Prussakad.java
+++ Prussakad/src/Prussakad.java
@@ -1,24 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Prussakad {
 	public static void main(String [] args){
-		Scanner vastus = new Scanner(System.in);
-		double raha = 2000.0;
-		Prussakas juss = Prussakas.loo_prussakas(1);
-		System.out.println("Tere, antud mängus peab tegema pakkumisi prussakate peale.");
-		System.out.println("Mäng saab läbi kui raha saab otsa, või oled võimeline ostma oma enda prussaka, mis maksab 5000");
-		System.out.println("Kas tahad alustada?")
-		String jahei = vastus.next();
-		if (jahei.equals("Jah")||jahei.equals("jah")){
-			System.out.println("Hakkame pihta, sul on "+ raha);
+		System.out.println("kartul");
+		Prussakas [] juss = new Prussakas[50];
+		for (int i = 0; i<50;i++){
+			juss[i] = Prussakas.loo_prussakas(1);
 		}
-		else{
-			System.out.println("Olgu, head aega");
-		}
+		for (Prussakas pruss : juss)System.out.println(pruss.getNimi());
 	}
 }
-
-
