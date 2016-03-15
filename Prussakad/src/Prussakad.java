6da261659b550fe9fbef58e3b99dc99d1eb0dcd2
markhenrip <markhenripedoson@gmail.com>
2016-03-15 22:06:11 +0200

    Scannering my ass

--- Prussakad/src/Prussakad.java
+++ Prussakad/src/Prussakad.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Prussakad {
 	public static void main(String [] args){
+		Scanner vastus = new Scanner(System.in);
 		System.out.println("kartul");
 		Prussakas [] juss = new Prussakas[50];
 		for (int i = 0; i<50;i++){
 			juss[i] = Prussakas.loo_prussakas(1);
 		}
 		for (Prussakas pruss : juss)System.out.println(pruss.getKiirus());
+		String jahei = vastus.next();
 	}
 }
